6ca9cb0df59d317a3f963b1ef4691f9e5f0d5ca4
Eyob Woldemichael <eyob@uwindsor.ca>
2022-08-06 19:55:39 -0400

    new files

--- artbeats/artbeats/urls.py
+++ artbeats/artbeats/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
 urlpatterns = [
     path('milli/', include('milli.urls')),
     path('admin/', admin.site.urls),
