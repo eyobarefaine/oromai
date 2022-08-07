68a0db3205e3ed0aab3769567ec22e2088a905ca
Eyob Woldemichael <eyob@uwindsor.ca>
2022-08-07 11:42:47 -0400

    new files

--- artbeats/artbeats/settings.py
+++ artbeats/artbeats/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 # Add these new lines
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
