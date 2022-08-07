3300d2731b9ad9299a00908e35b47fd6c18617b3
Eyob Woldemichael <eyob@uwindsor.ca>
2022-08-06 20:05:03 -0400

    new files

--- artbeats/artbeats/settings.py
+++ artbeats/artbeats/settings.py
@@ -125,7 +125,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
