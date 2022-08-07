970d621b0a01c405ac01f539108a383a1e912803
Eyob Woldemichael <eyob@uwindsor.ca>
2022-08-07 12:16:57 -0400

    new files

--- artbeats/milli/views.py
+++ artbeats/milli/views.py
@@ -4,6 +4,6 @@ from django.template import loader
 def index(request):
   template = loader.get_template('index.html')
   context = {
-    'title': 'Milli'
+    'title': 'Milli Artbeats'
   }
   return HttpResponse(template.render(context,request))
