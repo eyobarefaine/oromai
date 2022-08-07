62a672f2b488798ae1702faf58a2029dda7bf19f
Eyob Woldemichael <eyob@uwindsor.ca>
2022-08-07 12:22:23 -0400

    fix for the requirements

--- artbeats/milli/views.py
+++ artbeats/milli/views.py
@@ -4,6 +4,6 @@ from django.template import loader
 def index(request):
   template = loader.get_template('index.html')
   context = {
-    'title': 'Milli Artbeats'
+    'title': 'Milli Artbeats Page'
   }
   return HttpResponse(template.render(context,request))
