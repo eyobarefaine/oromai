b38a8ccbb25549588b6b048954e52c0ff3e24c2b
Eyob Woldemichael <eyob@uwindsor.ca>
2022-08-08 15:36:16 -0400

    add new def

--- artbeats/artbeats/urls.py
+++ artbeats/artbeats/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
 from milli import views
+from milli import aleka
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('',views.index, name='index'),
     path(r'^$', include('milli.urls')),
     path('astedadari/', include('milli.urls')),
-    path('aleka/',include('milli.urls')),
+    path('aleka/',aleka.index, name='index'),
     path('alekaw/',include('milli.urls')),
     path('admin/', admin.site.urls),
 ]
