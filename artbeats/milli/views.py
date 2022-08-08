45e96d027b3b3c05a6c3971efda7d71c21015a73
Eyob Woldemichael <eyob@uwindsor.ca>
2022-08-07 20:16:34 -0400

    fix for the requirements

--- artbeats/milli/views.py
+++ artbeats/milli/views.py
@@ -4,7 +4,15 @@ import psycopg2
 
 def index(request):
   conn = psycopg2.connect(database="artbeats", user="eyob", password="m1ll10n", host="0.0.0.0", port="5432")
-  print("Database Connected....")
+
+  cur = conn.cursor()
+  cur.execute("SELECT * from ARTS")
+
+  rows = cur.fetchall()
+  for row in rows:
+    print(row[0])
+
+  conn.close()
   template = loader.get_template('index.html')
   context = {
     'title': 'Milli Artbeats Page'
