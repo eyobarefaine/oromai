e579748541eac7f3b7be94bb65cecec1479bc85c
Eyob Woldemichael <eyob@uwindsor.ca>
2022-08-08 15:20:23 -0400

    add new def

--- artbeats/artbeats/urls.py
+++ artbeats/artbeats/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path('/', include('milli.urls')),
+    path(r'^$', include('milli.urls')),
     path('astedadari/', include('milli.urls')),
     path('aleka/',include('milli.urls')),
     path('alekaw/',include('milli.urls')),
