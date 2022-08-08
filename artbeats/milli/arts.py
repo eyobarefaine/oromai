c22e4e481ba55362f4e9cbf12eb78c2d45e1d6ed
Eyob Woldemichael <eyob@uwindsor.ca>
2022-08-07 20:42:53 -0400

    fix for the requirements

--- artbeats/milli/arts.py
+++ artbeats/milli/arts.py
@@ -1,7 +1,22 @@
+import psycopg2
 class Arts:
-    def __init__(self, name,age):
-        self.name=name
-        self.age =age
-    def art_func(self):
-      print(self.name)
+    def __init__(self, id):
+        self.id=id
+
+
+    def getArtName(self):
+        conn = psycopg2.connect(database="artbeats", user="eyob", password="m1ll10n", host="0.0.0.0", port="5432")
+
+        cur = conn.cursor()
+        cur.execute("SELECT * from ARTS where id ="+self.id)
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row[2])
+
+        conn.close()
+
+        return "all good"
+
 
--- artbeats/milli/views.py
+++ artbeats/milli/views.py
@@ -1,21 +1,13 @@
 from django.http import HttpResponse
 from django.template import loader
-import psycopg2
+
 from .arts import Arts
 def index(request):
 
-  arts = Arts("John",32)
-  arts.art_func()
-  conn = psycopg2.connect(database="artbeats", user="eyob", password="m1ll10n", host="0.0.0.0", port="5432")
+  arts = Arts(1)
+  a = arts.getArtName()
+  print (a)
 
-  cur = conn.cursor()
-  cur.execute("SELECT * from ARTS")
-
-  rows = cur.fetchall()
-  for row in rows:
-    print(row[2])
-
-  conn.close()
   template = loader.get_template('index.html')
   context = {
     'title': 'Milli Artbeats Page'
