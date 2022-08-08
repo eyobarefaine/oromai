b83c65a10f7615f796faab6b7bf4788bc83fa2bc
Eyob Woldemichael <eyob@uwindsor.ca>
2022-08-08 15:09:51 -0400

    add new def

---
+++ artbeats/milli/aleka.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.template import loader
+
+from .arts import Arts
+def index(request):
+
+  my = loader.get_template('astedadari.html')
+  context = {
+    'title': 'Milli Artbeats Page', 'artist': 'Eyob'
+  }
+  return HttpResponse(my.render(context, request))
--- artbeats/milli/urls.py
+++ artbeats/milli/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from . import aleka
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('', views.astedadari, name='astedadari'),
+    path('', aleka.index,name='index')
 ]
