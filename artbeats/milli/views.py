c772ac1c7f4f3491c80043f075858b3f8aaef209
Eyob Woldemichael <eyob@uwindsor.ca>
2022-08-07 20:12:15 -0400

    fix for the requirements

--- artbeats/milli/views.py
+++ artbeats/milli/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 from django.template import loader
 import psycopg2
-conn = psycopg2.connect(database="artbeats", user="eyob", password="m1ll10n", host="0.0.0.0", port="5432")
-print("Database Connected....")
+
 def index(request):
+  conn = psycopg2.connect(database="artbeats", user="eyob", password="m1ll10n", host="0.0.0.0", port="5432")
+  print("Database Connected....")
   template = loader.get_template('index.html')
   context = {
     'title': 'Milli Artbeats Page'
