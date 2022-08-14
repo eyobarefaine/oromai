5a5c2f38a9376722b9e6771275dc31fcbca48aa7
Eyob Woldemichael <eyob@uwindsor.ca>
2022-08-14 12:34:46 -0400

    edit

--- artbeats/milli/entity/model.py
+++ artbeats/milli/entity/model.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import UserManager
+#eyobwoldemichael   
 class arts(models.Model):
     title = models.TextField(default='')
     descr = models.TextField(default='')
