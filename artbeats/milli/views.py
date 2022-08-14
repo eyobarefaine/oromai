643737dcae89e893f8deeda210827fd7123d000b
Eyob Woldemichael <eyob@uwindsor.ca>
2022-08-14 15:23:48 -0400

    django form:

--- artbeats/milli/views.py
+++ artbeats/milli/views.py
@@ -1,14 +1,15 @@
 from django.http import HttpResponse
 from django.template import loader
 
-from .entity.model import arts
+from .entity.model import arts,artbeatproducts
 def index(request):
 
 
   obj = arts.objects.first()
+  products = artbeatproducts.objects.all()
   template = loader.get_template('index.html')
   context = {
-    'title': 'Milli Artbeats Page','artist':getattr(obj,'title'),'teaser':getattr(obj,'descr')
+    'title': 'Milli Artbeats Page','artist':getattr(obj,'title'),'teaser':getattr(obj,'descr'),'items':products
   }
 
   return HttpResponse(template.render(context,request))
