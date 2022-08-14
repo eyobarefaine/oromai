239761f0038175b19a2a089f680f2054203ffa14
Eyob Woldemichael <eyob@uwindsor.ca>
2022-08-14 08:58:56 -0400

    curd

--- artbeats/milli/curd.py
+++ artbeats/milli/curd.py
@@ -4,11 +4,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 
 def populate(self):
- # arts.objects.create(title="Million Woldemichael",descr="I Rock!")
- # artsadmin.objects.create(usersname='million',password='m1ll10n')
- #user = User.objects.create_user('million', 'million@gmail.com', 'm1ll10n')
- #artscategory.objects.create(category="tshirt",categorydescr="T-shirt")
- #artscategory.objects.create(category="bag", categorydescr="Hand bags")
+ arts.objects.create(title="Million Woldemichael",descr="I Rock!")
+ artsadmin.objects.create(usersname='million',password='m1ll10n')
+ user = User.objects.create_user('million', 'million@gmail.com', 'm1ll10n')
+ artscategory.objects.create(category="tshirt",categorydescr="T-shirt")
+ artscategory.objects.create(category="bag", categorydescr="Hand bags")
 
  return redirect ("/")
 
